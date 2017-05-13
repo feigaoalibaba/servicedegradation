e8ec92ce70890e25b0ed2252693745f60f55c372
feigaoalibaba <jiajingwen1984@163.com>
2017-05-13 13:45:48 +0800

    rename

--- src/test/java/com/bj58/MainTest.java
+++ src/test/java/com/bj58/MainTest.java
@@ -37,22 +37,5 @@ public class MainTest {
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-
-		
-	/*	for(int i = 0; i < 10; i++){
-			try{
-//				testControl.hello();
-//				System.out.println(testControl.sayHello());
-				List<TestBean> list = testControl.getNames();
-				for(TestBean bean: list){
-					System.out.println(bean.getId() + bean.getName() + bean.getCatList());
-				}
-//				TestBean bean = testControl.getBeans();
-//				System.out.println(bean.getId() + bean.getName() + bean.getCatList());
-			}catch(Exception e){
-				e.printStackTrace();
-			}	
-		}*/
-
 	}
 }
